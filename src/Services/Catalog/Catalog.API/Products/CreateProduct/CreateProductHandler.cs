c6ae7fd6f15e3bf8b310f4f45ed44a2393a207eb
Khalafallah55 <mohamedkhalafallah4@gmail.com>
2024-12-28 19:53:55 +0300

    End of Session 5

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,31 @@
+﻿
+using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductComamnd(string Name , List<string> Category , string Description , string ImageFile, decimal Price)
+        :ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductComamnd, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductComamnd command, CancellationToken cancellationToken)
+        {
+            //create product entity from command object
+            // save to database 
+            // return CreateProductResult resuly 
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+
+            //return result
+            return new CreateProductResult(Guid.NewGuid());
+
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+
+//add services to the container 
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
+var app = builder.Build();
+
+//configre the HTTP request pipeline
+app.MapCarter();
+
+app.Run();
